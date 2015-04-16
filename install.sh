f48d93ae4283a315a2a03fdac4fc4d08b4e1223f
moodern.devtrip <moodern@gmail.com>
2015-04-16 16:46:21 +0900

    install.sh Update

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 virtualenv --no-site-packages --distribute ~/.venv/abs
-source ~/.venv/abs/activate
+source ~/.venv/abs/bin/activate
 pip install -r pip-requirements.txt
 pip install http://www.parallelpython.com/downloads/pp/pp-1.6.4.tar.gz
